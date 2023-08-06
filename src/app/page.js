936ac82a0275cccdcf08222e428a430ce7c20b23
xvferdy <xv.ferdy@gmail.com>
2023-08-06 19:30:50 +0700

    💄 [UI]: set padding for desktop breakpoint

--- src/app/page.js
+++ src/app/page.js
@@ -8,100 +8,22 @@ import Testimonials from "@/components/Testimonials";
 import Accordion from "@/components/Accordion";
 
 export default function Home() {
-  // const kelas = [
-  //   {
-  //     id: 1,
-  //     slug: "MERN",
-  //     img: "mern.PNG",
-  //     name: "Programmer MERN",
-  //     desc: "Profesi yang bertugas membuat dan mengelola suatu aplikasi (React & Express)",
-  //   },
-  //   {
-  //     id: 2,
-  //     slug: "UIUX",
-  //     img: "uiux.PNG",
-  //     name: "UI/UX Designer",
-  //     desc: "UI/UX Designer profesi yang bertugas meriset dan membuat tampilan suatu aplikasi",
-  //   },
-  //   {
-  //     id: 3,
-  //     slug: "QA",
-  //     img: "qa.PNG",
-  //     name: "Quality Assurance",
-  //     desc: "Profesi yang bertugas melakukan pengecekan qualitas suatu aplikasi seperti error, alur, sistem dll",
-  //   },
-  //   {
-  //     id: 4,
-  //     slug: "DM",
-  //     img: "dm.PNG",
-  //     name: "Digital Marketing",
-  //     desc: "Profersi yang bertugas melakukan marketing dengan pendekatan serba digital",
-  //   },
-  //   {
-  //     id: 5,
-  //     slug: "laravel",
-  //     img: "laravel.PNG",
-  //     name: "Laravel Vue JS",
-  //     desc: "Profesi yang bertugas membuat dan mengelola suatu aplikasi (Vue.Js & Laravel)",
-  //   },
-  //   {
-  //     id: 6,
-  //     slug: "HR",
-  //     img: "hr.PNG",
-  //     name: "HR: Talent Acquisition",
-  //     desc: "Talent acquisition adalah proses memilih dan mempertahankan talenta potensial perusahaan",
-  //   },
-  // ];
-
-  // const alur = [
-  //   {
-  //     label: "1. Pendaftaran",
-  //     title: "Deskripsi Pendaftaran",
-  //     paragraph1:
-  //       "Lorem ipsum dolor sit amet consectetur. Suspendisse fringilla dictum velit ullamcorper rutrum in urna gravida. Duis quam aliquet bibendum dignissim adipiscing ac eu. Quis adipiscing sit aliquet pellentesque nulla nunc at ultrices. Vel volutpat in arcu gravida est nulla. Enim ut consectetur eget nisl duis sit sed dignissim.",
-  //     paragraph2:
-  //       "Lorem ipsum dolor sit amet consectetur. Quis adipiscing sit aliquet pellentesque nulla nunc at ultrices. Vel volutpat in arcu gravida est nulla. Enim ut consectetur eget nisl duis sit sed dignissim.",
-  //   },
-  //   {
-  //     label: "2. Pilih Program",
-  //     title: "Deskripsi Pilih Program",
-  //     paragraph1: "Lorem Suspendisse fringilla dictum velit ullamcorper.",
-  //     paragraph2: "",
-  //   },
-  //   {
-  //     label: "3. Pilih Kelas",
-  //     title: "Deskripsi Pilih Kelas",
-  //     paragraph1: "Velit ullamcorper rutrum in urna gravida.",
-  //     paragraph2: "",
-  //   },
-  //   {
-  //     label: "4. UpSkill",
-  //     title: "Deskripsi UpSkill",
-  //     paragraph1: "Rutrum in urna gravida. Duis quam aliquet bibendum.",
-  //     paragraph2: "",
-  //   },
-  //   {
-  //     label: "5. Project",
-  //     title: "Deskripsi Project",
-  //     paragraph1: "Duis quam aliquet bibendum dignissim adipiscing ac eu.",
-  //     paragraph2: "",
-  //   },
-  //   {
-  //     label: "6. Penyaluran Kerja",
-  //     title: "Deskripsi Penyaluran Kerja",
-  //     paragraph1: "Quis adipiscing sit aliquet pellentesque nulla at ultrices.",
-  //     paragraph2: "",
-  //   },
-  //   {
-  //     label: "7. Kerja",
-  //     title: "Deskripsi Kerja",
-  //     paragraph1: "Enim ut consectetur eget nisl duis sit sed dignissim.",
-  //     paragraph2: "",
-  //   },
-  // ];
   return (
     // <main className={`homepage-main ${openSans.className}`}>
     <main className="homepage-main">
+      {/* SECTION KELEBIHAN */}
+      <section className="kelebihan">
+        <div className="kelebihan__container">
+          <article>
+            <div className="header">
+              <span className="label-14-bold">ABOUT US</span>
+              <h2 className="heading-30-bold">Kenapa Memilih Kami</h2>
+            </div>
+            <div className="content">kelebihan</div>
+          </article>
+        </div>
+      </section>
+
       {/* SECTION WELCOME */}
       <section className="welcome">
         <div className="welcome__container">
