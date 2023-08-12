b4974123188043f75488fbac186e17219553532a
xvferdy <xv.ferdy@gmail.com>
2023-08-12 12:01:17 +0700

    🚧 [WIP]: alur soft transition

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Tabs({ alur }) {
   const [selectedTab, setSelectedTab] = useState("1. Pendaftaran");
@@ -22,16 +23,25 @@ function Tabs({ alur }) {
           </button>
         ))}
       </div>
-      {alur.map(
-        (el, idx) =>
-          el.label === selectedTab && (
-            <div key={idx} className="tabs__details">
-              <p className="tabs__details-title">{el.title}</p>
-              <p className="tabs__details-paragraph1">{el.paragraph1}</p>
-              <p className="tabs__details-paragraph2">{el.paragraph2}</p>
-            </div>
-          )
-      )}
+      <AnimatePresence mode="wait">
+        {alur.map(
+          (el, idx) =>
+            el.label === selectedTab && (
+              <motion.div
+                key={idx}
+                className="tabs__details"
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ type: "spring", duration: 0.5, bounce: 0.5 }}
+              >
+                <p className="tabs__details-title">{el.title}</p>
+                <p className="tabs__details-paragraph1">{el.paragraph1}</p>
+                <p className="tabs__details-paragraph2">{el.paragraph2}</p>
+              </motion.div>
+            )
+        )}
+      </AnimatePresence>
     </div>
   );
 }
