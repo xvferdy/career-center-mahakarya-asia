9d79af94e8157397e332a05777eeb8a118c71d0f
xvferdy <xv.ferdy@gmail.com>
2023-08-12 14:05:03 +0700

    🚧 [WIP]: alur soft transition

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -4,7 +4,20 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 function Tabs({ alur }) {
-  const [selectedTab, setSelectedTab] = useState("1. Pendaftaran");
+  const [selectedTab, setSelectedTab] = useState(1);
+  const [tuple, setTuple] = useState([null, selectedTab]);
+
+  if (tuple[1] !== selectedTab) {
+    setTuple([tuple[1], selectedTab]);
+  }
+
+  // console.log(tuple);
+
+  let prev = tuple[0];
+  let flow = selectedTab > prev ? "nextStep" : "prevStep";
+
+  console.log(flow);
+
   return (
     <div className="tabs">
       <div className="tabs__list">
@@ -13,27 +26,33 @@ function Tabs({ alur }) {
             key={idx}
             // className="selectedTab btn--chip"
             className={
-              el.label === selectedTab
+              el.id === selectedTab
                 ? "tab btn--chip btn--chip--active"
                 : "tab btn--chip"
             }
-            onClick={() => setSelectedTab(el.label)}
+            onClick={() => setSelectedTab(el.id)}
           >
             {el.label}
           </button>
         ))}
       </div>
-      <AnimatePresence mode="wait">
+      <AnimatePresence mode="wait" custom={flow}>
         {alur.map(
           (el, idx) =>
-            el.label === selectedTab && (
+            selectedTab === idx + 1 && (
               <motion.div
                 key={idx}
                 className="tabs__details"
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                transition={{ type: "spring", duration: 0.5, bounce: 0.5 }}
+                // initial={{ opacity: 0, y: 10 }}
+                // animate={{ opacity: 1, y: 0, x: "1px" }}
+                // exit={{ opacity: 0, y: -10 }}
+                // transition={{ type: "spring", duration: 0.5, bounce: 0.5 }}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                custom={flow}
+                // transition={{ duration: 1 }}
               >
                 <p className="tabs__details-title">{el.title}</p>
                 <p className="tabs__details-paragraph1">{el.paragraph1}</p>
@@ -45,5 +64,19 @@ function Tabs({ alur }) {
     </div>
   );
 }
+
+let variants = {
+  enter: (flow) => ({
+    opacity: 0,
+    x: flow === "nextStep" ? 100 : -100,
+    y: "2px",
+  }),
+  center: { opacity: 1, x: 0, y: "2px" },
+  exit: (flow) => ({
+    opacity: 0,
+    x: flow === "nextStep" ? -100 : 100,
+    y: "2px",
+  }),
+};
 
 export default Tabs;
